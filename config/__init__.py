a961defb7fba772163e299990f51819c4e1ffc19
aihub472rgb <aigithub472@gmail.com>
2026-09-04 01:40:27 +0530

    Add config package init

---
+++ config/__init__.py
@@ -0,0 +1,11 @@
+"""
+Configuration package for PDF OCR Converter.
+"""
+from .settings import Settings
+from .paths import Paths
+from .constants import *
+
+__all__ = [
+    'Settings',
+    'Paths',
+]
